33e4d6497b891183cb131d1cdac4c8d62492fa90
NicoleBarbuzzi <39517275+NicoleBarbuzzi@users.noreply.github.com>
2021-03-14 12:25:42 +0100

    Added final

--- StaticFaultTreeRepeatedEvents/src/CDF/ExponentialDistributionCDF.java
+++ StaticFaultTreeRepeatedEvents/src/CDF/ExponentialDistributionCDF.java
@@ -3,7 +3,7 @@ package CDF;
 
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 
-public class ExponentialDistributionCDF implements CDFInterface{
+public final class ExponentialDistributionCDF implements CDFInterface{
     private ExponentialDistribution exp;
     
     public ExponentialDistributionCDF(double lambda){
--- StaticFaultTreeRepeatedEvents/src/CDF/NormalDistributionCDF.java
+++ StaticFaultTreeRepeatedEvents/src/CDF/NormalDistributionCDF.java
@@ -3,7 +3,7 @@ package CDF;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
 
-public class NormalDistributionCDF implements CDFInterface{
+public final class NormalDistributionCDF implements CDFInterface{
     private NormalDistribution normal;
 
     public NormalDistributionCDF(double mu, double sigma) {
