6a2e2e9e24ceb78aec422693ce11d489167bbefd
NicoleBarbuzzi <39517275+NicoleBarbuzzi@users.noreply.github.com>
2021-03-14 12:24:35 +0100

    Added getPobabilityFault of KoutNGate

--- StaticFaultTreeRepeatedEvents/src/staticfaulttree/KoutNGate.java
+++ StaticFaultTreeRepeatedEvents/src/staticfaulttree/KoutNGate.java
@@ -1,5 +1,6 @@
 package staticfaulttree;
 
+import static java.lang.Math.pow;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -10,15 +11,19 @@ import java.util.List;
  * @author Minarelli
  */
 public final class KoutNGate extends Gate{
+    private List<Node> children = new ArrayList<>();
+    public int k;
     
-    
-    public KoutNGate(){};
+    public KoutNGate(int k){
+        k = this.k;
+    }
     
     /**
      * Constructor 
      * @param c List of nodes that are OR inputs.
      */
-    public KoutNGate(List<Node> c){
+    public KoutNGate(List<Node> c, int k){
+        k = this.k;
         Node n;
         for(Iterator<Node> i = c.iterator(); i.hasNext(); ){
             n= i.next();
@@ -35,8 +40,31 @@ public final class KoutNGate extends Gate{
      */
     @Override
     public double getProbabilityFault(double t) {
-       //TODO
-       return 0;
+       double rSys = 0; //Reliability of system
+       double rCdf = 1; //Reliability of single component
+       int n = children.size();
+       int nFatt = 1; //var di appoggio per n!
+       for(int i=1; i<=n; i++){
+           nFatt = nFatt * i;
+       }
+       
+       for(int i = k; i<=n; i++){
+           int inFatt = 1;
+           for(int j=1; j<=(n-i); j++){
+               inFatt = inFatt * j;
+           }
+           int iFatt = 1;
+           for(int m=1; m<=i; m++){
+               iFatt = iFatt*m;
+           }
+           
+           int bin = nFatt/((inFatt)*(iFatt));
+           rCdf = children.get(i).getProbabilityFault(t);
+           
+           rSys = rSys + bin*pow((1-rCdf),i)*pow(rCdf,(n-i));   
+       }
+       
+       return (1 - rSys);
     }
     
 }
